bac2bd8855738e410764f9054d0204ed0e1483e2
MSulSal <msulsaleem@gmail.com>
2025-04-19 18:03:10 -0500

    add staggered grid effect

--- contents.js
+++ contents.js
@@ -11,4 +11,8 @@ const contents = [
     name: "Twitch Hover Effect",
     link: "https://msulsal.github.io/twitch-hover-effect/",
   },
+  {
+    name: "Staggered Grid Effect",
+    link: "https://msulsal.github.io/staggered-grid-effect/",
+  },
 ];
