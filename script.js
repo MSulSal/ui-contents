d9e6357d88f4cce0966e0bfa97abaac6c0d97026
MSulSal <MSulSal@users.noreply.github.com>
2025-04-20 03:02:04 +0000

    deploy: dd2c95a2bef3f9ae19d4481744b30df6c093bb66

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+const wrapper = document.getElementById("tiles");
+const tileSize = 50;
+
+const colors = [
+  "rgb(229, 57, 53)",
+  "rgb(244, 81, 30)",
+  "rgb(253, 216, 53)",
+  "rgb(76, 175, 80)",
+  "rgb(33, 150, 243)",
+  "rgb(156, 39, 176)",
+];
+
+let columns = 0;
+let rows = 0;
+let toggled = false;
+
+const handleOnClick = (index) => {
+  toggled = !toggled;
+  anime({
+    targets: ".tile",
+    opacity: toggled ? 0 : 1,
+    delay: anime.stagger(50, {
+      grid: [columns, rows],
+      from: index,
+    }),
+  });
+};
+
+const createTile = (index) => {
+  const tile = document.createElement("div");
+
+  tile.classList.add("tile");
+  tile.onclick = (e) => handleOnClick(index);
+  return tile;
+};
+
+const createTiles = (quantity) => {
+  Array.from(Array(quantity)).map((tile, index) => {
+    wrapper.appendChild(createTile(index));
+  });
+};
+
+const createGrid = () => {
+  wrapper.innerHTML = "";
+  columns = Math.floor(document.body.clientWidth / tileSize);
+  rows = Math.floor(document.body.clientHeight / tileSize);
+
+  wrapper.style.setProperty("--columns", columns);
+  wrapper.style.setProperty("--rows", rows);
+
+  createTiles(columns * rows);
+};
+createGrid();
+window.onresize = () => createGrid();
+
 const lists = document.getElementsByTagName("ul");
 for (const list of lists) {
   for (let i = 0; i < contents.length; i++) {
@@ -9,13 +64,13 @@ for (const list of lists) {
 
 const left = document.getElementById("left-side");
 
-const handleOnMove = (e) => {
-  const p = (e.clientX / window.innerWidth) * 100;
-  left.style.width = `${p}%`;
-};
+// const handleOnMove = (e) => {
+//   const p = (e.clientX / window.innerWidth) * 100;
+//   left.style.width = `${p}%`;
+// };
 
-document.onmousemove = (e) => handleOnMove(e);
-document.ontouchmove = (e) => handleOnMove(e.touches[0]);
+// document.onmousemove = (e) => handleOnMove(e);
+// document.ontouchmove = (e) => handleOnMove(e.touches[0]);
 
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("network-canvas");
